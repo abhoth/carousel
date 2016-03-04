f0cd5f0d797f7314fad0914c8f2315332f581f92
Paul Zane <paul@zanesworld.net>
2016-03-04 00:12:44 -0500

    Code tweaks, new images and added debounce plugin

--- js/carousel.js
+++ js/carousel.js
@@ -7,11 +7,12 @@ function Carousel(name,slider,sliderNav,animationSpeed, autoSpeed){
     this.autoSpeed = autoSpeed;
 
     var _this = this;
-    var sliderWidth = slider.width();
-    var slideWidth =  sliderWidth /  this.slider.children().length;
-    var offSet;
-    var timer;
-	
+    var sliderWidth,
+        slideWidth,
+        offSet,
+        timer,
+	    resizeTimeout;
+		
     this.animateSlider = function(offSet){
         this.slider.animate({
             left: offSet + 'px'
@@ -37,7 +38,7 @@ function Carousel(name,slider,sliderNav,animationSpeed, autoSpeed){
         }
         else{
             offSet = 0;
-            this.sliderNav.find('a:first-child').addClass('on');
+            this.sliderNav.find('a:first').addClass('on');
         }
 
         this.animateSlider(offSet);
@@ -54,63 +55,61 @@ function Carousel(name,slider,sliderNav,animationSpeed, autoSpeed){
         }
         else {
             offSet = -(sliderWidth - slideWidth);
-            this.sliderNav.find('a:last-child').addClass('on');
+            this.sliderNav.find('a:last').addClass('on');
         }
 
         this.animateSlider(offSet);
     }
 	
 	this.time = function(){
-		
-		
-	    
-				
-				
-					
-				
-		
-			    timer = setInterval(function(){
+		 timer = setInterval(function(){
 				_this.nextSlide();
-				}, autoSpeed);
-				
-		
+		  }, autoSpeed);
 		
 	}
+	
 	this.init = function(){
 		
+		    resizeTimeout = false;
+		    offSet = 0;
+		    sliderWidth = slider.width();
+            slideWidth =  sliderWidth /  this.slider.children().length;
+	        clearInterval(timer);
+			
+			this.sliderNav.children().removeClass('on');
+            this.sliderNav.find('a:first').addClass('on');
+	        this.slider.css('left',offSet + 'px');
+			
 			this.sliderNav.find('a').on('click', function(e){
 				
 				e.preventDefault;
 				_this.carouselSlide($(this));
 				clearInterval(timer);
 				_this.time();
-				});
-			this.sliderNav.find('input:first-child').on('click', function(e){
+		     });
+			 
+			this.sliderNav.find('input:first').on('click', function(e){
 				
 				e.preventDefault;
 				_this.previousSlide();
 				clearInterval(timer);
 				_this.time();
-				});
+		     });
 			
-			this.sliderNav.find('input:last-child').on('click', function(e){
+			this.sliderNav.find('input:last').on('click', function(e){
 				
 				e.preventDefault;
 				_this.nextSlide();
 				clearInterval(timer);
 				_this.time();
 				
-				});
-			$(window).on('resize', function(e){
-				
-			    sliderWidth = slider.width();
-                slideWidth =  sliderWidth /  _this.slider.children().length;
-				clearInterval(timer);
-   				_this.time();
-				});
+		     });
 			
-			
-				_this.time();
+			 $(window).smartresize(function(){
+  				_this.init();
+			   });
+						
+		    _this.time();
 				
 				
 			
